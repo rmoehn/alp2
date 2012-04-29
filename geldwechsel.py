1daa8e9b92f37e4742ef82b2f9ecb0061727935f
Richard Möhn <richard.moehn@fu-berlin.de>
2012-04-29 11:59:10 +0200

    Geldwechselfunktion für 02.03 rekursiv

---
+++ geldwechsel.py
@@ -0,0 +1,38 @@
+COINS = [200, 100, 50, 20, 10, 5, 2, 1]
+
+def change_money_rec(amount, coins=COINS):
+    """
+    Berechnet die günstigste Kombination von Münzen für einen Geldbetrag
+
+    - rekursive Variante
+
+    Parameter:
+        - amount ... Geldbetrag in Cents
+        - coins  ... Liste der möglichen Münzen in Centäquivalent von groß
+                     nach klein
+    Rückgabe:
+        Liste der den Geldbetrag bildenden Münzen in Centäquivalent
+
+    """
+    # If there is no money left to be divided into coins...
+    if amount == 0:
+        return []
+
+    # If remaining amount of money is too small to be expressed in coins...
+    if coins == [] and amount > 0:
+        # ...die.
+        raise Exception("Restbetrag kleiner als die kleinste Münze!")
+
+    # If currently biggest coin fits into the amount of money...
+    if coins[0] <= amount:
+        # Put the coin into the list of returned coins and proceed with the
+        # remaining money
+        return [ coins[0] ] + change_money_rec( amount - coins[0], coins )
+
+    # If currently biggest coin does not fit into the amount of money...
+    if coins[0] > amount:
+        # Remove the biggest coin from the list of available coins
+        coins.pop(0)
+
+        # Proceed with the old amount of money and the new set of coins
+        return change_money_rec(amount, coins)
