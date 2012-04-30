373df12be4545e1a5cfa6046aff64df2fa4d6c83
Richard Möhn <richard.moehn@fu-berlin.de>
2012-04-30 17:07:32 +0200

    Whitespace-Korrekturen in Espondas Mosaic_Gui

--- Mosaic_GUI.py
+++ Mosaic_GUI.py
@@ -2,17 +2,17 @@
 
 """
 	This script creates a window on your screen and visualizes your mosaics.
-	
+
 	Please, don't manipulate this code. It's really not necessary to change
 	anything.
-	
+
 	You also don't need to understand all the details of the functions
 	on this script to write your solutions.
-	
+
 	If you want to place more than six mosaics on your window you just need
 	to add more calls of the 'create_mosaic' function in the main function,
 	each one with a new name of your 'decide_color' function.
-	
+
 	Please ask your tutor in your exercise session if you want to learn
 	more detail about this script.
 """
@@ -26,7 +26,7 @@ def create_mosaic(graphic, stone_size, decide_color):
            Each piece has the same 'stone_size'."""
 	size = int(graphic.cget("height"))
 	s = size//stone_size #number of rows, which is equal to the number of columns
-	
+
 	# for each (row, col) position the decide_color function is called
 	for row in range(s):
 		for col in range(s):
@@ -41,34 +41,34 @@ def create_mosaic(graphic, stone_size, decide_color):
 			graphic.create_rectangle(x1, y1, x2, y2, fill = color, outline="")
 
 	# makes the graphic visible
-	graphic.pack()	
+	graphic.pack()
 
 def create_graphic(size):
 	"""Creates the graphic object in which the mosaic is painted"""
 	return Canvas(root, width = size, height = size)
-	
+
 def main():
 
 	# size of the whole picture and size of each stone of the picture
 	size = 160
 	stone_size = 1
-	
+
 	# creates a window for the mosaics
-	global root 
+	global root
 	root = Tk()
 	root.title("Mosaics")
-	
+
 	# first mosaic
 	pic_one = create_graphic(size)
 	create_mosaic(pic_one, stone_size, color_functions.decide_color_rects)
 	# sets the position of the first mosaic in the window
 	pic_one.grid(row = 0, column = 0)
-	
+
 	# second mosaic
 	pic_two = Canvas(root, width = size, height = size)
 	create_mosaic(pic_two, stone_size, color_functions.decide_color_circle)
 	pic_two.grid(row = 0, column = 1)
-	
+
 	# third mosaic
 	pic_three = Canvas(root, width = size, height = size)
 	create_mosaic(pic_three, stone_size, color_functions.decide_color_squares)
@@ -83,7 +83,7 @@ def main():
 	pic_three = Canvas(root, width = size, height = size)
 	create_mosaic(pic_three, stone_size, color_functions.decide_color_illusion2)
 	pic_three.grid(row = 1, column = 1)
-	
+
 	# own mosaic 1
 	pic_own = Canvas(root, width = size, height = size)
 	create_mosaic(pic_own, stone_size, color_functions.decide_color_own1)
@@ -93,10 +93,10 @@ def main():
 	pic_own = Canvas(root, width = size, height = size)
 	create_mosaic(pic_own, stone_size, color_functions.decide_color_own2)
 	pic_own.grid(row = 2, column = 0)
-	
+
 	# shows the picture permanently
 	root.mainloop()
-	
+
 main()
 
 
