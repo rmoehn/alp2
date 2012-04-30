829d37cc97be350f18ee7ce329c3e32bf8cd2349
Richard Möhn <richard.moehn@fu-berlin.de>
2012-04-30 17:07:48 +0200

    Ein paar mehr Farbspielereien fertig

--- color_functions.py
+++ color_functions.py
@@ -17,10 +17,25 @@
                'http://tmml.sourceforge.net/doc/tk/colors.html'
 """
 from math import sqrt
+import random
+random.seed()
 
 def decide_color_rects(x, y, size):
-        # write your code here
+    bigsq_size = size // 3       # Seitenlänge des Quadrates in der Mitte
+    smlsq_size = bigsq_size // 2 # Seitenlänge des kleine schwarzen Quadrats
+
+    # Ganz großes schwarzes Quadrat in die untere rechte Ecke setzen
+    if bigsq_size + smlsq_size <= x \
+        and bigsq_size + smlsq_size <= y:
         return "black"
+
+    # Großes rosanes Quadrat darunter in die Mitte setzen
+    if bigsq_size <= x < 2 * bigsq_size \
+        and bigsq_size <= y < 2 * bigsq_size:
+        return "pink"
+
+    # Rest schwarz
+    return "black"
 
 def decide_color_circle(x, y, size):
         # write your code here
@@ -33,12 +48,6 @@ def decide_color_squares(x, y, size):
 def decide_color_illusion(x, y, size):
     sq_size = size // 8    # Kantenlänge der kleinen Quadrate
     ra_size = sq_size // 5 # Breite der kleinen Rechtecke
-
-    """
-    Man könnte alle ifs mit ors verbinden, aber dann wird es noch
-    unübersichtlicher.
-
-    """
 
     # Alle senkrechten Linien malen. Die Bedingung ist notwendig,
     # damit nur jeweils an den Kanten der Rechtecke Linien gezeichnet und
@@ -72,11 +81,27 @@ def decide_color_illusion(x, y, size):
     return "white"
 
 def decide_color_illusion2(x, y, size):
+    bigsq_size = size // 3        # Seitenlänge des dicken lilanen Quadrates
+    smlsq_size = size // 20       # Seitenlänge der kleinen Quadrate
+                                  # einschließlich der Linie links und unten
+    line_width = smlsq_size // 4  # Breite der weißen Linien
+
+    # Dickes lilanes Quadrat in der Mitte zeichnen
+    if bigsq_size <= x < 2 * bigsq_size \
+        and bigsq_size <= y < 2 * bigsq_size:
         return "magenta"
 
+    # Gitter der weißen Linien zeichnen
+    if x % smlsq_size < line_width \
+        or y % smlsq_size < line_width:
+        return "white"
+
+    # Rest schwarz
+    return "black"
+
 def decide_color_own1(x, y, size):
-        return "gray"
+    return random.choice(["gray", "gray", "gray", "gray", "gray", "gray",
+            "gray", "gray", "gray", "gray", "gray", "gray", "white"])
 
 def decide_color_own2(x, y, size):
-        return "gray"
-
+    return random.choice(["black", "black", "red", "red", "red", "green"])
