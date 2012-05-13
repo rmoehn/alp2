9d87665ab2163649f8731c51678773d3841a3b86
Richard Möhn <richard.moehn@fu-berlin.de>
2012-05-13 15:37:09 +0200

    Endlich Quicksort geschafft!

--- sorting.py
+++ sorting.py
@@ -47,3 +47,69 @@ def random_list(my_seed=None):
             # Obfuscation even in Python
 
     return randlist
+
+
+def qsort(array, lower_ind=None, upper_ind=None):
+    """
+    Sortiert eine (Teil-)Liste mit dem Quicksort-Algorithmus
+
+    Als Pivot-Element wird hier nicht jeweils das erste, sondern das mittlere
+    Element einer Teilliste verwendet.
+
+    Parameter:
+        array ... zu sortierende Liste
+        lower_ind ... linker Index des zu sortierenden Teils
+        upper_ind ... rechter Index des zu sortiernden Teils
+
+    """
+
+    # On first call set proper parameters
+    if upper_ind == lower_ind == None:
+        qsort(array, 0, len(array) - 1)
+        return
+
+    # As long as partial list length is not 1
+    if lower_ind < upper_ind:
+        # Partition current array part
+        old_pivot_ind = partition_m(array, lower_ind, upper_ind)
+
+        # Sort the partitions separately
+        qsort(array, lower_ind,         old_pivot_ind - 1)
+        qsort(array, old_pivot_ind + 1, upper_ind)
+
+    return
+
+
+def partition_m(array, lower_ind, upper_ind):
+    """
+    Sortiert eine (Teil-)Liste für den Quicksort-Algorithmus um
+
+    Das mittlere Elemente der (Teil-)Liste wird als Pivot-Element gewählt und
+    aller größeren Elemente dahinter, alle kleineren Elemente davor
+    angeordnet.
+
+    """
+
+    # Move pivot element from the middle of the array to the front
+    pivot_ind = lower_ind + (upper_ind - lower_ind) // 2
+    pivot     = array[pivot_ind]
+    array[lower_ind], array[pivot_ind] = array[pivot_ind], array[lower_ind]
+
+    # Index of the element that is exchanged in exchange steps
+    exch_ind = lower_ind
+
+    # Walk through assigned part of the array
+    for ind in range(lower_ind + 1, upper_ind + 1):
+        # Move elements smaller than the pivot element to the front
+        if array[ind] < pivot:
+            exch_ind += 1
+            array[ind], array[exch_ind] = array[exch_ind], array[ind]
+
+    # Replace the pivot in the (new) middle
+    array[lower_ind], array[exch_ind] = array[exch_ind], array[lower_ind]
+
+    # Return boundary for subsequent calls
+    return exch_ind
+
+
+testlist = [14, 33, 89, 87, 68, 56, 40, 26, 96, 73]
