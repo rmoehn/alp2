125c62d16b627f337be9203f876e0708a83ffc11
Richard Möhn <richard.moehn@fu-berlin.de>
2012-05-14 20:18:00 +0200

    Programm mit kleinsten Differenzen fertig

---
+++ nahe_zahlen.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3.2
+"""
+From a list of numbers returns those two with the smallest difference
+
+If there is more than one pair with the same smallest difference, the one with
+the smallest numbers is returned.
+
+"""
+
+from sorting import bsort
+from sys import argv
+
+def find_smallest_diff_pair(numbers):
+    """
+    Does exactly what the whole program does, on subroutine level
+
+    """
+
+    # Sort input list
+    numbers = bsort(numbers)
+        # To many exercises to use and analyse a more efficient algorithm
+
+    # Walk through adjacent numbers and determine least difference with flood
+    # peak algorithm
+    # Initialize flood peak with first pair
+    least_difference_yet = numbers[1] - numbers[0]
+    least_tuple_yet      = (numbers[0], numbers[1])
+    for i in range(0, len(numbers) - 1):
+        cur_difference = numbers[i+1] - numbers[i]
+
+        # Update least pair yet if smaller pair found
+        if 0 != cur_difference < least_difference_yet:
+            least_tuple_yet = (numbers[i], numbers[i+1])
+
+    return least_tuple_yet
+
+
+# If called as a program, work on the command line arguments
+if __name__ == '__main__':
+    print(
+        find_smallest_diff_pair(
+            list(map(
+                float, # That's ok, I think.
+                argv[1:]
+            ))
+        )
+    )
