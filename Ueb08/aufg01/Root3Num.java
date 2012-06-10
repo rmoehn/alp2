97f7fcfafef77323a4d605d0cc55855155fcd45a
Richard Möhn <richard.moehn@fu-berlin.de>
2012-06-10 11:14:38 +0200

    Schon viel zu viel gemacht

--- Ueb08/aufg01/Root3Num.java
+++ Ueb08/aufg01/Root3Num.java
@@ -24,11 +24,77 @@ public class Root3Num {
     }
 
     /**
+     * Add Root3Num object to this.
+     *
+     * @param that  a Root3Num to add to this
+     * @return a new Root3Num being the sum of this and that
+     */
+    public Root3Num add(Root3Num that) {
+        return new Root3Num(this.a + that.a, this.b + that.b);
+    }
+
+
+    /**
+     * Multiply this Root3Num by an integer.
+     *
+     * @param factor   an int this Root3Num is multiplied by
+     * @return  a new Root3Num
+     */
+    public Root3Num times(int factor) {
+        return new Root3Num(factor * this.a, factor * this.b);
+    }
+
+
+    /**
+     * Subtract a Root3Num from this.
+     *
+     * @param that   a Root3Num to subtract from this
+     * @return a new Root3Num being the difference between this and that
+     */
+    public Root3Num sub(Root3Num that) {
+        return new Root3Num(this.a - that.a, this.b - that.b);
+            // Could be done with this + (-1)*that, but I reckon that's slower
+    }
+
+
+    /**
      * Return the number's real value as double.
      *
      * @return  a double holding the number's calculated value
      */
     public double value() {
         return this.a + this.b * Math.sqrt(3);
+    }
+
+
+    /**
+     * Test whether an object is a Root3Num and equal to this.   Two Root3Nums
+     * are equal iff their coefficients are equal.
+     *
+     * @param o   Object to be tested
+     * @return    true if other object is equal to this, false otherwise
+     */
+    public boolean equals(Object o) {
+        // Test whether the other is a valid Root3Num
+        if ( o == null || !o.getClass().equals(this.getClass()) ) {
+            return false;
+        }
+
+        // Compare the two Root3Nums' coefficient
+        Root3Num that = (Root3Num) o;
+        return this.a == that.a && this.b == that.b;
+    }
+
+
+    /**
+     * Override the generic <code>hashCode</code> method.   The hash code for
+     * the two ints representing the Root3Num is calculated according to the
+     * guidelines in @see.
+     *
+     * @return  an int being the hash code for this Root3Num
+     * @see <a href="http://www.javamex.com/tutorials/collections/hash_function_guidelines.shtml">http://www.javamex.com/tutorials/collections/hash_function_guidelines.shtml</a>
+     */
+    public int hashCode() {
+        return (this.a * 33) ^ this.b;
     }
 }
--- Ueb08/aufg01/Root3NumTest.java
+++ Ueb08/aufg01/Root3NumTest.java
@@ -2,12 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class Root3NumTest {
+    private Root3Num num_null;
+    private Root3Num num_one;
     private Root3Num num1;
     private Root3Num num2;
+
     private double delta = 0.000_000_1;
 
     // Create objects to run the tests on
     @Before public void setUp() {
+        num_null = new Root3Num(0, 0);
+        num_one  = new Root3Num(1, 1);
         num1 = new Root3Num(21, 4);
         num2 = new Root3Num(-12, 7);
     }
@@ -16,5 +21,44 @@ public class Root3NumTest {
     @Test public void test_value() {
         assertEquals(num1.value(), 27.92820323, delta);
         assertEquals(num2.value(), 0.124355653, delta);
+    }
+
+    // Test method times()
+    @Test public void test_times() {
+        assertEquals(new Root3Num(84, 16), num1.times(4));
+        assertEquals(new Root3Num(12, -7), num2.times(-1));
+        assertEquals(num_null,             num2.times(0));
+        assertEquals(num1,                 num1.times(1));
+    }
+
+    // Test instance method add()
+    @Test public void test_inst_add() {
+        assertEquals(new Root3Num(9, 11), num1.add(num2));
+        assertEquals(new Root3Num(9, 11), num2.add(num1));
+        assertEquals(num1, num1.add(num_null));
+    }
+
+    // Test instance method sub()
+    @Test public void test_inst_sub() {
+        assertEquals(new Root3Num(33, -3), num1.sub(num2));
+        assertEquals(new Root3Num(-33, 3), num2.sub(num1));
+        assertEquals(num2, num2.sub(num_null));
+    }
+
+    // Test equals() method
+    @Test public void test_equals() {
+        assertEquals(num1, num1);
+        assertEquals(num1, new Root3Num(21, 4));
+        assertEquals(new Root3Num(21, 4), num1);
+
+        assertTrue(!num1.equals(num2));
+        assertTrue(!num1.equals(null));
+        assertTrue(!num1.equals(new Object()));
+    }
+
+    // Test hashCode() method
+    @Test public void test_hashcode() {
+        assertEquals(num1.hashCode(), num1.hashCode());
+        assertEquals(num1.hashCode(), new Root3Num(21, 4).hashCode());
     }
 }
