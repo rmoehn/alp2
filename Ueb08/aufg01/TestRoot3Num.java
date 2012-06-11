1f4a16b7875db5b0e0545482ef946b7b06696158
Richard Möhn <richard.moehn@fu-berlin.de>
2012-06-11 15:55:13 +0200

    Angefangen mit TestRoot3Num

---
+++ Ueb08/aufg01/TestRoot3Num.java
@@ -0,0 +1,14 @@
+/**
+ * Rather silly class for "testing" the rather silly Root3Num class.
+ */
+public class TestRoot3Num {
+    public static main() {
+        Root3Num[][] matr1 = {
+    {new Root3Num( 99,  37), new Root3Num( 74, -54), new Root3Num(-80, -93)},
+    {new Root3Num( 34,  56), new Root3Num(-20, -49), new Root3Num(-32,  19)},
+    {new Root3Num(-97, -44), new Root3Num(-84,  -2), new Root3Num(-15, -26)},
+    {new Root3Num( 88,  69), new Root3Num( 75,  17), new Root3Num(-21,  80)},
+    {new Root3Num(-61,  16), new Root3Num(-55, -94), new Root3Num(-59, -38)},
+        };
+
+}
