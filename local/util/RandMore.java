3c4d20498f153be50eabbbb014b62cf831cd0a92
Richard Möhn <richard.moehn@fu-berlin.de>
2012-06-15 17:19:08 +0200

    Versuch eines Werkzeug-Packages

---
+++ local/util/RandMore.java
@@ -0,0 +1,19 @@
+package shapes;
+
+import java.util.Random;
+
+public class RandMore extends Random {
+    /**
+     * Returns an integral random number in the range a to b-1.
+     */
+    public int randrange(int a, int b) {
+        return this.nextInt(b - a) + a;
+    }
+
+    /**
+     * Returns a double random number in the range a to b.
+     */
+    public double dblrange(double a, double b) {
+        return this.nextDouble() * (b - a) + a;
+    }
+}
