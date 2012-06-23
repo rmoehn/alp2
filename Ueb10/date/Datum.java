4aec6a85fb7abe70f79268014d6534d35216bee0
Richard Möhn <richard.moehn@fu-berlin.de>
2012-06-23 20:32:34 +0200

    add ziemlich viel von der Datum-Klasse und Tests dazu

---
+++ Ueb10/date/Datum.java
@@ -0,0 +1,144 @@
+package date;
+
+/**
+ * Provides an object for simple date operations.
+ */
+public class Datum {
+    private int year;
+    private int month;
+    private int day;
+
+    /**
+     * Constructs a new <code>Datum</code> object.   Year, month and day have
+     * to be given in ISO order.
+     *
+     * @param year  the year of the date (negative numbers for years BCE)
+     * @param month the month of the date
+     * @param day   the day of the date
+     *
+     * @throws IllegalDateException If an invalid date was given
+     */
+    public Datum(int year, int month, int day) throws IllegalDateException {
+        // Die on ominous month
+        if (month < 1 || month > 12) {
+            throw new IllegalDateException(
+                          "Invalid month number.",
+                          year, month, day
+                      );
+        }
+
+        // Die on ominous day
+        if (day < 1 || day > days_in_month(month, year)) {
+            throw new IllegalDateException(
+                          "Invalid day of month.",
+                          year, month, day
+                      );
+        }
+
+        // Set this Datum's fields
+        this.year  = year;
+        this.month = month;
+        this.day   = day;
+    }
+
+    /**
+     * Changes this <code>Datum</code> to the day after it.
+     */
+    public void nextDay() throws IllegalDateException {
+        // Current day is last day of month...
+        if (this.day == days_in_month(this.month, this.year)) {
+            // ...and last day of year: Set to start of following year.
+            if (this.month == 12) {
+                ++this.year;
+                this.month = 1;
+                this.day   = 1;
+            }
+            // ...and midst in the year: Set to start of following month.
+            else {
+                ++this.month;
+                this.day = 1;
+            }
+        }
+        // Current day is ordinary day: Set to following day.
+        else {
+            ++this.day;
+        }
+    }
+
+    /**
+     * Determine the number of days in a month in a certain year.
+     *
+     * @param month  the number of the month
+     * @param year   the year (negative numbers for years BCE)
+     *
+     * @return the number of days in the month month in the year year
+     *
+     * @throws IllegalDateException If the number of the month is silly
+     */
+    public static int days_in_month(int month, int year)
+           throws IllegalDateException {
+        switch (month) {
+            case  1: /* falls through */
+            case  3: /* falls through */
+            case  5: /* falls through */
+            case  7: /* falls through */
+            case  8: /* falls through */
+            case 10: /* falls through */
+            case 12:
+                return 31; // Make sure to insert breaks upon removing return.
+            case  4: /* falls through */
+            case  6: /* falls through */
+            case  9: /* falls through */
+            case 11: /* falls through */
+                return 30;
+            case  2:
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    // Leap year
+                    return 29;
+                }
+                else {
+                    return 28;
+                }
+            default:
+                throw new IllegalDateException(
+                              "Invalid month number.",
+                              year, month, 0
+                          );
+        }
+    }
+
+    /**
+     * Test whether an object is a <code>Datum</code>  and equal to this.
+     * Two <code>Datum</code>s are equal iff their year, month and date are
+     * equal.
+     *
+     * @param o   Object to be tested
+     * @return    <code>true</code> if other object is equal to this,
+     *            <code>false</code> otherwise
+     */
+    public boolean equals(Object o) {
+        // Test whether the other is a Datum at all
+        if ( o == null || !o.getClass().equals(this.getClass()) ) {
+            return false;
+        }
+
+        // Compare the two Datums' year, month, day
+        Datum that = (Datum) o;
+        return this.year  == that.year
+            && this.month == that.month
+            && this.day   == that.day;
+    }
+
+    /**
+     * Override the generic <code>hashCode</code> method.   The hash code for
+     * the three ints representing the <code>Datum</code> is calculated
+     * according to the guidelines in @see.
+     *
+     * @return  an int being the hash code for this Datum
+     * @see <a href="http://www.javamex.com/tutorials/collections/hash_function_guidelines.shtml">http://www.javamex.com/tutorials/collections/hash_function_guidelines.shtml</a>
+     */
+    public int hashCode() {
+        return (this.year * 33) ^ (this.month * 37) ^ this.day;
+    }
+
+}
---
+++ Ueb10/date/DatumTest.java
@@ -0,0 +1,146 @@
+package date;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Class for testing the class <code>Datum</code>.
+ */
+public class DatumTest {
+    Datum dat1;
+
+    @Before public void setUp() throws IllegalDateException {
+        dat1 = new Datum(1968, 7, 10);
+    }
+
+    // Tests for the days_in_month() class method
+    @Test(expected=IllegalDateException.class)
+    public void test_days_in_month_exc_1() throws IllegalDateException {
+        Datum.days_in_month(0, 2700);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_days_in_month_exc_2() throws IllegalDateException {
+        Datum.days_in_month(-60, 3);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_days_in_month_exc_3() throws IllegalDateException {
+        Datum.days_in_month(13, 999);
+    }
+
+    @Test public void test_days_in_month() throws IllegalDateException {
+        assertEquals(Datum.days_in_month(4, 1930), 30);
+        assertEquals(Datum.days_in_month(10, 1517), 31);
+        assertEquals(Datum.days_in_month(2, 1620), 29);
+        assertEquals(Datum.days_in_month(2, 1200), 29);
+        assertEquals(Datum.days_in_month(2, 1700), 28);
+        assertEquals(Datum.days_in_month(2, 1749), 28);
+        assertEquals(Datum.days_in_month(2, 0), 29);
+        assertEquals(Datum.days_in_month(2, -32), 29);
+        assertEquals(Datum.days_in_month(2, -100), 28);
+    }
+
+    // Tests for exceptions on constructing invalid Datum's
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_1() throws IllegalDateException {
+        Datum bla = new Datum(1777, 0, 23);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_2() throws IllegalDateException {
+        Datum bla = new Datum(9, 15, 12);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_3() throws IllegalDateException {
+        Datum bla = new Datum(1096, 6, 31);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_4() throws IllegalDateException {
+        Datum bla = new Datum(769, 2, 29);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_5() throws IllegalDateException {
+        Datum bla = new Datum(23_500, 10, -4);
+    }
+
+    @Test(expected=IllegalDateException.class)
+    public void test_new_datum_exc_6() throws IllegalDateException {
+        Datum bla = new Datum(1958, 5, 35);
+    }
+
+
+    // Tests for method nextDay()
+    @Test public void test_nextDay() throws IllegalDateException {
+        Datum datum;
+
+        // Dieses beschissene Interface ist absolut testfeindlich!
+
+        // Tageswechsel
+        datum = new Datum(1912, 6, 22);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1912, 6, 23));
+        datum = new Datum(-44, 3, 14);
+        datum.nextDay();
+        assertEquals(datum, new Datum(-44, 3, 15));
+
+        // Monatswechsel
+        datum = new Datum(1992, 5, 30);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1992, 5, 31));
+        datum = new Datum(1992, 5, 31);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1992, 6, 1));
+        datum = new Datum(1872, 4, 30);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1872, 5, 1));
+        datum = new Datum(1834, 8, 30);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1834, 8, 31));
+
+        // Jahreswechsel
+        datum = new Datum(2001, 12, 31);
+        datum.nextDay();
+        assertEquals(datum, new Datum(2002, 1, 1));
+        datum = new Datum(-1, 12, 31);
+        datum.nextDay();
+        assertEquals(datum, new Datum(0, 1, 1));
+        datum = new Datum(-754, 12, 31);
+        datum.nextDay();
+        assertEquals(datum, new Datum(-753, 1, 1));
+
+        // Schaltjahr oder nicht Schaltjahr
+        datum = new Datum(1900, 2, 28);
+        datum.nextDay();
+        assertEquals(datum, new Datum(1900, 3, 1));
+        datum = new Datum(2000, 2, 28);
+        datum.nextDay();
+        assertEquals(datum, new Datum(2000, 2, 29));
+        datum = new Datum(2012, 2, 28);
+        datum.nextDay();
+        assertEquals(datum, new Datum(2012, 2, 29));
+        datum = new Datum(2012, 2, 29);
+        datum.nextDay();
+        assertEquals(datum, new Datum(2012, 3, 1));
+    }
+
+    // Test equals() method
+    @Test public void test_equals() throws IllegalDateException {
+        assertEquals(dat1, dat1);
+        assertEquals(dat1, new Datum(1968, 7, 10));
+        assertEquals(new Datum(1968, 7, 10), dat1);
+
+        assertTrue(!dat1.equals(new Datum(1967, 1, 29)));
+        assertTrue(!dat1.equals(null));
+        assertTrue(!dat1.equals(new Object()));
+    }
+
+    // Test hashCode() method
+    @Test public void test_hashcode() throws IllegalDateException {
+        assertEquals(dat1.hashCode(), dat1.hashCode());
+        assertEquals(dat1.hashCode(), new Datum(1968, 7, 10).hashCode());
+    }
+}
